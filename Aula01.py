3dda2e5bb2d600671313493ca5c51dab6c0ea161
marcoscustodiodias <marcosdias.pvai@gmail.com>
2023-01-12 11:43:22 -0300

    Inclusão de Label Kivy

--- Aula01.py
+++ Aula01.py
@@ -1,9 +1,15 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Button(text='Clique para Confirmar')
+        #return Button(text='Clique para Confirmar')
+        label = Label(text= 'Ola Mundo')
+        label.fonte_size = 50
+        return label
+        
+
 
 
 
