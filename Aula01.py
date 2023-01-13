d27f703599da80de76adafa43300976e48df54da
marcoscustodiodias <marcosdias.pvai@gmail.com>
2023-01-13 17:02:11 -0300

    Botão em Kivy

--- Aula01.py
+++ Aula01.py
@@ -3,12 +3,23 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
+def on_press(btn):
+    btn.text = 'Apertado'
+
+def on_release(btn):
+    btn.text = 'Solto'    
+   
+
 class MyApp(App):
     def build(self):
-        box = BoxLayout()
+        box = BoxLayout(orientation='vertical')
         label = Label(text= 'Ola Mundo')
         label.font_size = 50
-        btn = Button(text='Button')
+        btn = Button(
+            text='Button',
+            on_press=on_press,
+            on_release=on_release,
+            )
         btn.font_size = 50
         
         box.add_widget(label)
